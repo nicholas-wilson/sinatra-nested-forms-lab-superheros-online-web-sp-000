7f671b0d98eac5b5bc8d741a4228ca0ab4fa8db2
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-07 23:03:52 +0000

    Automatically backed up by Learn

---
+++ app/models/hero.rb
@@ -0,0 +1,14 @@
+class Hero
+  attr_accessor :name, :power, :bio
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @power = attributes[:power]
+    @bio = attributes[:biography]
+  end
+
+  def self.make_heroes(heroes_hash)
+    heroes_hash.each do |hero|
+      
+  end
+end
