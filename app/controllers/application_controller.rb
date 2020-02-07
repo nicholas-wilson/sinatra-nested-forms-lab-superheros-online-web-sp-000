7ebaa96ea51db072ac9ca6a68cfbb0225f76b623
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-07 23:24:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   end
 
   post '/teams' do
-    @team = Team.new(params[:team])
-    " #{@team.heroes}"
+    # @team = Team.new(params[:team])
+    " #{params[:team]}"
   end
 end
