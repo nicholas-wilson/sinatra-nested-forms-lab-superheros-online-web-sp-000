a5fd4abd53c3ee102d1dcfb315a48cdcfa1caa73
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-07 23:29:19 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -14,9 +14,7 @@ class Hero
       guy = Hero.new(hero_info)
       guy.team = team
     end
-    the_team = Hero.all.collect do |hero|
-      hero.team == team
-    end
+    the_team = Hero.all.collect {|x| if x.team == team}
     the_team
   end
 
